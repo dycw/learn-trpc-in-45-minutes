fa4935184e2748e9b3fcc202f68dd78c96808f68
Derek Wan <d.wan@icloud.com>
2023-06-12 22:52:25 +0800

    Implement more (#4)

--- backend/api.ts
+++ backend/api.ts
@@ -1,10 +1,17 @@
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import cors from "cors";
 import express from "express";
+import { createContext } from "./context";
 import { appRouter } from "./routers";
 
 const app = express();
 app.use(cors({ origin: "http://localhost:5173" }));
-app.use("/trpc", createExpressMiddleware({ router: appRouter }));
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 app.listen(3000);
 console.log("Express server listening to http://localhost:3000");
--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -1,7 +1,20 @@
+import { z } from "zod";
 import { t } from "../trpc";
 
+const userProcedure = t.procedure.input(z.object({ userId: z.string() }));
+
 export const userRouter = t.router({
-  getUser: t.procedure.query(() => {
-    return { id: 1, name: "Kyle" };
+  get: userProcedure.query(({ input }) => {
+    return { id: input.userId };
   }),
+  update: userProcedure
+    .input(z.object({ name: z.string() }))
+    .output(z.object({ name: z.string(), id: z.string() }))
+    .mutation((req) => {
+      console.log(req.ctx.isAdmin);
+      console.log(
+        `Updating user ${req.input.userId} to have name ${req.input.name}`
+      );
+      return { id: req.input.userId, name: req.input.name };
+    }),
 });
