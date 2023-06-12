5aa1f66948436a2411597e11a61864b2f0aba1a2
Derek Wan <d.wan@icloud.com>
2023-06-12 23:12:14 +0800

    Try websocket

--- backend/context.ts
+++ backend/context.ts
@@ -1,5 +1,3 @@
-import type { CreateExpressContextOptions } from "@trpc/server/adapters/express";
-
-export function createContext({ req, res }: CreateExpressContextOptions) {
-  return { req, res, isAdmin: true };
+export function createContext() {
+  return { isAdmin: true };
 }
