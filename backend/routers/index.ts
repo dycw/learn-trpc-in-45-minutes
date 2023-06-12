a20c2f8dafb08de9334629969abc476f0643d116
Derek Wan <d.wan@icloud.com>
2023-06-12 22:51:31 +0800

    Implement more

--- backend/routers/index.ts
+++ backend/routers/index.ts
@@ -1,4 +1,4 @@
-import { t } from "../trpc";
+import { adminProcedure, t } from "../trpc";
 import { userRouter } from "./users";
 
 export const appRouter = t.router({
@@ -17,6 +17,10 @@ export const appRouter = t.router({
       console.log(`Client says: ${req.input}`);
       return true;
     }),
+  secretData: adminProcedure.query(({ ctx }) => {
+    console.log(ctx.user);
+    return "Super top secret admin data";
+  }),
   users: userRouter,
 });
 
--- frontend/client.ts
+++ frontend/client.ts
@@ -14,8 +14,15 @@ async function main() {
   console.log(result1);
   const result2 = await client.log.mutate("Hi from client");
   console.log(result2);
-  const result3 = await client.users.getUser.query();
+  const result3 = await client.users.get.query({ userId: "foobar" });
   console.log(result3);
+  const result4 = await client.users.update.mutate({
+    userId: "foobar",
+    name: "New_Name",
+  });
+  console.log(result4);
+  const result5 = await client.secretData.query();
+  console.log(result5);
 }
 
 main();
